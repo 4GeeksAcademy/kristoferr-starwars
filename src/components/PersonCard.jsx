9def5042385420f5887c6413ba0f52d61a23ff42
Kristofer Reynolds <kristofertreynolds@gmail.com>
2026-04-04 20:16:36 +0000

    finished dynamic personcard

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const PersonCard = (person) => {
@@ -7,11 +7,12 @@ export const PersonCard = (person) => {
 
     return(
         <div className="card m-4" style={{width: "18rem"}}>
-            <img src="https://github.com/breatheco-de/swapi-images/blob/master/public/images/people/1.jpg?raw=true" className="card-img-top" />
+            <img src={person.person.image} className="card-img-top" />
             <div className="card-body">
                 <h5 className="card-title">{person.person.name}</h5>
-                {console.log(person)}
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                <div className="card-gender">Gender: {person.person.gender}</div>
+                <div className="card-hair">Hair-Color: {person.person.hair}</div>
+                <div className="card-eyes">Eye-Color: {person.person.eyes}</div>
                 <button className="btn btn-primary">Learn More</button>
             </div>
         </div>
--- src/store.js
+++ src/store.js
@@ -4,12 +4,20 @@ export const initialStore=()=>{
       {
         uid: 1,
         name: "Luke Skywalker",
-        url: "https://www.swapi.tech/api/people/1"
+        url: "https://www.swapi.tech/api/people/1",
+        image: "https://github.com/breatheco-de/swapi-images/blob/master/public/images/people/1.jpg?raw=true",
+        gender: "male",
+        hair: "blond",
+        eyes: "blue"
       },
       {
-        "uid": "2",
-        "name": "C-3PO",
-        "url": "https://www.swapi.tech/api/people/2"
+        uid: "2",
+        name: "C-3PO",
+        url: "https://www.swapi.tech/api/people/2",
+        image: "https://github.com/breatheco-de/swapi-images/blob/master/public/images/people/2.jpg?raw=true",
+        gender: "robot",
+        hair: "none",
+        eyes: "yellow"
       }
     ]
   }
